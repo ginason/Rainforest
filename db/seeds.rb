767939af20ac616655224aa1059e2b81c2fb0e6f
Gina Son <ginasunah33@gmail.com>
2017-07-24 20:26:39 -0400

    Adding some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(
+name: 'Calendar',
+description: 'Yearly calendar at the bottom of each sheet',
+price_in_cents: 1200
+)
+Product.create(
+name: 'Ball Pens',
+description: 'Needle point tip.',
+price_in_cents: 1699
+)
+Product.create(
+name: 'Umbrella',
+description: 'designed with 9 reinforced fiverglass ribs',
+price_in_cents: 2299
+)
