2e0ecade5be219493c7ada0ab97b4e93bfb2aa4b
stein328i <stein.328i@gmail.com>
2017-07-25 15:57:33 -0400

    update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,21 +8,30 @@ class ProductsController < ApplicationController
   end
 
   def new
-
+    @product = Product.new
   end
 
   def create
+      @product = Product.new(product_params)
+
+      if @product.save
+        redirect_to products_path
+      else
+        redirect_to new_product_path
+      end
+    end
+
+    def destroy
+      @product = Product.find(params[:id])
+      @product.destroy
+      redirect_to products_path
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
 
   end
-
-  def edit
-
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
-end
